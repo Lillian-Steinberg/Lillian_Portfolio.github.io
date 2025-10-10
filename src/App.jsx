a5cc3ba2c59b6ae3e658d0d9f70f49618af60dc8
Lillian-Steinberg <katherinelilliansmith@gmail.com>
2025-10-10 18:46:52 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
     <>
       <Navbar />
       <HeroSection />
+      <Projects />
       <About />
       <Skill />
-      <Projects />
       <Resume />
       <Footer />
     </>
